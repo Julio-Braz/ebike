40b9fdc327dfb06d4ace5fd6405086dec3f21f66
Julio-Braz <juliobraz@id.uff.br>
2022-12-06 19:52:37 -0300

    video 3/ 7 min: 20:10

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,14 +1,44 @@
 import { Box } from "native-base";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DetailBackground } from "../../atomic/atoms/DetailBackground";
 import MainBanner from "../../atomic/molecules";
+import Card from "../../atomic/molecules/Card";
+import api from "../../service/api";
 
+
+type CardProps = {
+    title: string;
+    price: number;
+    model: string;
+    image: string;
+    id: string;
+}
 
 export function Home(){
+
+    const [equipments, setequipments]= useState<CardProps[]>([])
+
+    useEffect (() => {
+        async function getEquipments( ) {
+            try{
+                const {data} = await api.get('equipments')
+                              
+                setequipments(data)
+
+            }catch(err){
+                console.log(err)
+            }
+            
+        }
+        getEquipments()
+    },[])
+
     return(
     <Box flex= '1' padding='20px' pt='-10px' position='relative'>
         <DetailBackground/>
         <MainBanner/>
+        {console.log(equipments)}
+        <Card/>
     </Box>
     )
 }
