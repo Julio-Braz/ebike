57614771915b0e110d4e51256febc6395a72c5bf
Julio-Braz <juliobraz@id.uff.br>
2022-12-20 16:59:43 -0300

    Criando os primeiros passos do footer

--- Readme.md
+++ Readme.md
@@ -1,4 +1,32 @@
-👉 IMPORTANTE 
+Projeto E-bike, projeto feito no canal @cantodocodigo.
+
+https://www.youtube.com/@cantodocodigo
+
+____________________________________________________
+
+
+Como baixar o projeto:
+
+  ##
+  clonar o repositorio(Link do criador)
+
+  $ git clone https://github.com/RgMellon/video-e-bike.git
+
+  ##
+  entrar na pasta do projeto
+  $ cd video-e-bike
+
+  ##
+  Instalar as dependencias
+  $ yarn
+
+  ##
+  Basta ler o codigo de barras que aparecerá, com isso o projeto deverá abrir
+  $ expo start
+
+_________________________
+
+👉Links importantes
 🔗 Link da config dos temas : https://gist.github.com/RgMellon/5a0b...
 
 🔗 Link dos assets  : https://drive.google.com/file/d/106uD...
--- src/page/Detail/index.tsx
+++ src/page/Detail/index.tsx
@@ -1,10 +1,11 @@
 import { useRoute } from "@react-navigation/native";
-import { Text, Box, Image } from "native-base";
+import {  Box, Image, HStack, Text, Button } from "native-base";
 import React, { useEffect, useState } from "react";
-import { SafeAreaView } from "react-native";
 import api from "../../service/api";
 import { CardProps } from "../Home";
 import rectangle from '../../img/rectangle.png'
+import { ImagePreview } from "../../atomic/atoms/ImagePreview";
+import { ButtonDetail } from "../../atomic/atoms/ButtonDetail";
 
 type RouteParams = {
     equipmentId: string;    
@@ -31,7 +32,7 @@ export function Detail (){
     }, []);
     
     return(
-        <SafeAreaView>
+    <>
             <Box 
                 flex='1'
                 position='relative'
@@ -46,7 +47,87 @@ export function Detail (){
                 right='0'
                 bottom='0'
             />
+            <ImagePreview 
+            showDescription={false} 
+            image={equipment.image}
+            alt='Image Detail'/>
+              
             </Box>
-        </SafeAreaView>
-    )
+
+             {false? (
+             <Box 
+                bg='primary.100'
+                justifyContent='center'
+                p='7'
+                borderRadius={30}
+                shadow={5}
+                h='110px'
+
+            >
+                <HStack w='100%' justifyContent= 'space-between'>
+                    <ButtonDetail title='Description'/>
+                    <ButtonDetail title='Specification'/>
+                </HStack>
+            </Box> 
+            ) : (
+                <Box 
+                    bg='primary.100' 
+                    borderTopRadius={30} 
+                    shadow={5}
+                     h='440px'
+                 >
+                <Box pl='7' p='7' pt='7'>
+                    <HStack w='100%' justifyContent= 'space-between'>
+                        <ButtonDetail title='Description'/>
+                        <ButtonDetail title='Specification'/>
+                    </HStack>
+                    <Box mt='30'>
+                        <Text bold color='white' fontSize='17'>
+                            {equipment.title}                       
+                        </Text>
+
+                        <Text color='white' opacity='0.6' mt='8'>
+                        orem ipsum dolor sit amet. Et odit vero ut dolor similique et voluptatibus numquam. Et dolores amet et corporis saepe cum veritatis expedita et adipisci voluptas sed ipsum reprehenderit.orem ipsum do.
+
+            
+                        </Text>
+                        {/* gerar um flatlist para percorrer melhor a descrição. */}
+                    </Box>
+                            
+                </Box>
+                <HStack 
+                    bg='primary.200'
+                    borderRadius={30}
+                    justifyContent='space-between'
+                    shadow={5}
+                    h={100}
+                    w={'100%'}
+                    mt='30'
+                    alignItems='center'
+                    p='7'
+                    >
+                    <Text bold color= 'ocean.200' fontSize='17'
+                         >${equipment.price}
+                    </Text>
+                                          
+                        <Button 
+                        w='149'
+                        h='43'
+                        justifyContent='center'
+                        alignItems='center'
+                        borderRadius='10px'
+                        shadow={5}
+                        bg='ocean.200'
+                        _pressed={{bgColor: 'ocean.100'}}
+                        onPress={() => {}}
+                        >
+                            Add to cart
+                        </Button>
+            
+                </HStack> 
+
+            </Box>
+            )}                       
+      </>  
+    );
 }
